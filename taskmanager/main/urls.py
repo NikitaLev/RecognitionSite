6e1916d96c6228e63471059e8d23838a502e7217
MikitaLev <mikita.leventsyev@mail-cyber.com>
2023-05-26 17:05:31 +0300

    Fix

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path('upload_audio', views.upload_audio, name='upload_audio'),
     path('out_audio', views.out_audio, name='out_audio'),
     path('stt', views.stt, name='stt'),
+    path('see', views.see, name='see'),
     path('tts', views.tts, name='tts'),
     path('test', views.test, name='test'),
     path('test2', views.test2, name='test2')
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -13,41 +13,8 @@ logger = logging.getLogger(__name__)
 
 
 def index(request):
-    #print('!!!!!!!!!', response.text)
-    if request.method == 'POST':
-        audio = Song(request.POST, request.FILES)
-        if audio.is_valid():
-            lang = audio.cleaned_data['lang']
-            model = audio.cleaned_data['model']
-            version = audio.cleaned_data['version']
-            sample_rate = audio.cleaned_data['sample_rate']
-            
-            config = {
-                "model": "{}".format(model),
-                "version": version,
-                "lang": "{}".format(lang),
-                "sample_rate": sample_rate
-            }
-            print(request)
-            file = request.FILES['file'].read()
-            data = {
-                'config':config,
-                'file': file
-            }
-            response = requests.post('http://web_api-flask-1:5000/v1/stt', files={'file': file}, data=config)
-            data = {'src': file,
-                    'text': response.text}
-            return render(request, "out_audio.html", data)
-        else:
-            data = {'text': 'error'}
-            return render(request, "out_audio.html",data)
-    else:
-        student = Song()
-        return render(request, "index.html", {'form': student})
+    return render(request, "index.html")
 
-
-def upload_audio(request):
-    return render(request, 'upload_audio.html')
 
 def test(request):
     data = {
@@ -97,13 +64,13 @@ def tts(request):
             return render(request, "out_audio.html",data)
     else:
         student = Song()
-        return render(request, "index.html", {'form': student})
+        return render(request, "upload_audio.html", {'form': student})
 
 
 def stt(request):
     if request.method == 'GET':
         student = ConfigTTS()
-        return render(request, "index.html", {'form': student})
+        return render(request, "upload_audio.html", {'form': student})
     else:
         url = "http://web_api-flask-1:5000/v1/tts"
         logger.critical('nor this')
@@ -122,7 +89,7 @@ def stt(request):
         logger.critical('model '+choice2 )
         logger.critical('version '+choice3 )
         logger.critical('text '+text )
-        #config = {"lang": choice1, "model": choice2, "version": choice3}
+
         payload={'config': '{{"lang": "{}", "model": "{}", "version": "{}" }}'.format(choice1,choice2,choice3),
         'text': text}
 
@@ -136,26 +103,7 @@ def stt(request):
             os.makedirs(src)
         ct=response.content
         filename = handle_uploaded_file(ct)
-        #with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as f:
-            #f.write(response.content)
-            #filename = f.name
-        """src = 'data/wav/'
-        if not os.path.exists(src):
-            os.makedirs(src)
-        name = uuid.uuid4().__str__()
-        temp = tempfile.TemporaryFile(suffix='.wav')
-        temp.write(response.content.decode())
-        file = temp.read()
-        temp.close()
-        logger.critical("file")
-        logger.critical(file)
-        data = {
-            'url' : file,
-            'text': payload['text'],
-        }
-        logger.critical("data")
-        logger.critical(data)"""
-        
+
         data = {
             'src' : filename,
             'text': payload['text'],
@@ -164,26 +112,35 @@ def stt(request):
         logger.critical(data)
         return render(request, "out_audio.html", data)
 
-        """jsonC = {
-            "model": "speecht5",
-            "version": 1,
-            "lang": "en",
-            "sample_rate": 16000
-        }
-        #print('!!!!!!!!!', response.text)
-        if request.method == 'POST':
+
+def see(request):
+    if request.method == 'POST':
+        audio = Song(request.POST, request.FILES)
+        if audio.is_valid():
+            lang = audio.cleaned_data['lang']
+            model = audio.cleaned_data['model']
+            version = audio.cleaned_data['version']
+            sample_rate = audio.cleaned_data['sample_rate']
+            
+            config = {
+                "model": "{}".format(model),
+                "version": version,
+                "lang": "{}".format(lang),
+                "sample_rate": sample_rate
+            }
             print(request)
-            audio = Song(request.POST, request.FILES)
-            if audio.is_valid():
-                file = request.FILES['file'].read()
-                data = {
-                    'json_param':jsonC,
-                    'file': file
-                }
-                response = requests.post('http://web_api-flask-1:5000/tts', files={'file': file}, data=jsonC)
-                data = {'src': file,
-                        'text': response.text}
-                return render(request, "out_audio.html", data)
+            file = request.FILES['file'].read()
+            data = {
+                'config':config,
+                'file': file
+            }
+            response = requests.post('http://web_api-flask-1:5000/v1/see', files={'file': file}, data=config)
+            data = {'src': file,
+                    'text': response.text}
+            return render(request, "out_audio.html", data)
         else:
-            student = Song()
-            return render(request, "index.html", {'form': student})"""
+            data = {'text': 'error'}
+            return render(request, "out_audio.html",data)
+    else:
+        student = Song()
+        return render(request, "upload_audio.html", {'form': student})
